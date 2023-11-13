c93df7d4cd05bb9da12ce2c70493848a3783e17f
dpprdgls <daps.douglas@gmail.com>
2023-11-13 11:57:58 -0800

    Update server.js
    uncomment

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require("express-handlebars");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const routes = require("./routes");
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 const sequelize = require("./config/connection");
 
 const app = express();
