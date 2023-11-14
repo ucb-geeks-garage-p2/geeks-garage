b490581edc639867c96cda90f5cd2e03ba364468
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-11-13 21:00:51 -0600

    remove main task.js

--- models/Task.js
+++ models/Task.js
@@ -1,45 +1,43 @@
+const { Model, Sequelize, DataTypes } = require("sequelize");
 
-const { Model, Sequelize, DataTypes } = require('sequelize');
- main
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
-class Task extends Model { }
-
+class Task extends Model {}
 
 Task.init(
-    {
-        id: {
-            type: DataTypes.UUID,
-            allowNull: false,
-            primaryKey: true,
-            defaultValue: DataTypes.UUIDV4
-        },
-        task_name: {
-            type: DataTypes.STRING,
-        },
-        created_on: {
-            type: DataTypes.BIGINT,
-            allowNull: false,
-          },
-        due_by: {
-            type: DataTypes.BIGINT,
-            allowNull: true
-          },
-        car_id: {
-            type: DataTypes.UUID,
-            references: {
-                model: 'car',
-                key: 'id',
-            }
-        }
+  {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
     },
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'task',
-    }
+    task_name: {
+      type: DataTypes.STRING,
+    },
+    created_on: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    due_by: {
+      type: DataTypes.BIGINT,
+      allowNull: true,
+    },
+    car_id: {
+      type: DataTypes.UUID,
+      references: {
+        model: "car",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "task",
+  }
 );
 
 module.exports = Task;
