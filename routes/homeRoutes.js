40a547369a30a44ada6a00517227e279059cb619
Koby <kobysac@gmail.com>
2023-11-18 00:38:31 -0800

    fixed small bug, need to have working login/signup buttons

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -8,67 +8,68 @@ router.get('/', async (req, res) => {
 
     console.log(usersWithCars);
 
-    res.render('userpage', {  });
-  }
-  // console.log('************** not logged in *************');
-  
-  const loginObj = {
-    message: req.session.lastMessage,
-    isLogin: true,
-    failedLogin: req.session.failedLogin ,
-    failedSignUp: req.session.failedSignUp
-  }
+    res.render('userpage', {});
+  } else {
+    // console.log('************** not logged in *************');
 
-  res.render('login-test', loginObj);
+    const loginObj = {
+      message: req.session.lastMessage,
+      isLogin: true,
+      failedLogin: req.session.failedLogin,
+      failedSignUp: req.session.failedSignUp
+    }
+
+    res.render('login-test', loginObj);
+  }
 });
 
 router.post('/login', async (req, res) => {
   try {
-      const user = await userController.checkUserByEmail(req.body.email);
+    const user = await userController.checkUserByEmail(req.body.email);
 
-      if (!user) {
-          req.session.lastMessage = "Incorrect email or password, please try again";
-          res.render('login-test', { failedLogin: true, message: req.session.lastMessage });
-          return;
-      }
+    if (!user) {
+      req.session.lastMessage = "Incorrect email or password, please try again";
+      res.render('login-test', { failedLogin: true, message: req.session.lastMessage });
+      return;
+    }
 
-      const validPassword = await user.checkPassword(req.body.password);
+    const validPassword = await user.checkPassword(req.body.password);
 
-      if (!validPassword) {
-          req.session.lastMessage = "Incorrect email or password, please try again";
-          res.render('login-test', { failedLogin: true, message: req.session.lastMessage });
-          return;
-      }
+    if (!validPassword) {
+      req.session.lastMessage = "Incorrect email or password, please try again";
+      res.render('login-test', { failedLogin: true, message: req.session.lastMessage });
+      return;
+    }
 
-      await req.session.save(() => {
-          req.session.loggedIn = true;
-          req.session.userID = user.id;
-          console.log(
-              'File: user-routes.js ~ req.session.save ~ req.session.cookie',
-              req.session.cookie
-          );
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      req.session.userID = user.id;
+      console.log(
+        'File: user-routes.js ~ req.session.save ~ req.session.cookie',
+        req.session.cookie
+      );
 
-          req.session.lastView = 'home';
+      req.session.lastView = 'home';
 
-          req.session.lastMessage = "your in the mainframe!";
+      req.session.lastMessage = "your in the mainframe!";
 
-          // console.log("---user logged in---");
-          res.status(200).json(user);
-      });
+      // console.log("---user logged in---");
+      res.status(200).json(user);
+    });
   } catch (err) {
-      console.log(err);
-      req.session.failedSignUp = false;
-      req.session.failedLogin = true;
-      res.status(500).json(err);
+    console.log(err);
+    req.session.failedSignUp = false;
+    req.session.failedLogin = true;
+    res.status(500).json(err);
   }
 })
 
 router.post('/logout', async (req, res) => {
   if (req.session.loggedIn) {
-      req.session.destroy(() => {
-          // console.log("---user logged out---");
-          res.render('login-test');
-      });
+    req.session.destroy(() => {
+      // console.log("---user logged out---");
+      res.render('login-test');
+    });
   } else {
     res.render('login-test');
   }
@@ -76,7 +77,7 @@ router.post('/logout', async (req, res) => {
 
 
 router.get('/more/:id', async (req, res) => {
-  
+
 })
 
 
