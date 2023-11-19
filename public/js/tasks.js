caa94dff9257ce4e17c62be7f6cb68ada1886861
dpprdgls <74167696+dpprdgls@users.noreply.github.com>
2023-11-18 21:23:20 -0800

    add changes to userpage handlebars

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,60 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const createTaskButton = document.getElementById('createTaskButton');
-  const createTaskModal = new bootstrap.Modal(document.getElementById('createTaskModal'));
-  const createTaskForm = document.getElementById('createTaskForm');
-  const carIdInput = document.getElementById('carId'); // Assuming the ID of the carId input field
-
-  if (createTaskButton) {
-    createTaskButton.addEventListener('click', () => {
-      // Get the id of the first car in the array
-      const firstCarIdElement = document.querySelector('ul li:first-child .car-id-input');
-      if (firstCarIdElement) {
-        const firstCarId = firstCarIdElement.innerText.trim();
-        // carIdInput.value = firstCarId; // Remove this line to prevent setting the value in the input field
-      }
-
-      // Show the modal
-      createTaskModal.show();
-    });
-  }
-
-  if (createTaskForm) {
-    createTaskForm.addEventListener('submit', async (event) => {
-      event.preventDefault();
-
-      const taskName = document.getElementById('taskName').value.trim();
-      const createdOn = Date.now();
-      const dueBy = document.getElementById('dueBy').value.trim();
-      const carId = document.getElementById('carId').value.trim(); // This will still send the value in the request
-
-      try {
-        const response = await fetch('/', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            task_name: taskName,
-            created_on: createdOn,
-            due_by: dueBy,
-            car_id: carId, // This value will be sent in the request
-          }),
-        });
-
-        if (response.ok) {
-        //   const newTask = await response.json();
-        document.location.replace('/');
-          console.log('New task created:', newTask);
-          // Handle success, e.g., show a success message or redirect to another page
-          createTaskModal.hide(); // Hide the modal after successful creation
-        } else {
-          console.error('Failed to create task:', response.status, response.statusText);
-          // Handle failure, e.g., show an error message to the user
+    const createTaskButton = document.getElementById('createTaskButton');
+    const createTaskModal = new bootstrap.Modal(document.getElementById('createTaskModal'));
+    const createTaskForm = document.getElementById('createTaskForm');
+  
+    if (createTaskButton) {
+      createTaskButton.addEventListener('click', () => {
+        createTaskModal.show();
+      });
+    }
+  
+    if (createTaskForm) {
+      createTaskForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+  
+        console.log('createTaskForm hit');
+  
+        const taskName = document.getElementById('taskName').value.trim();
+        const createdOn = Date.now();
+        const dueBy = document.getElementById('dueBy').value.trim();
+        const carId = document.getElementById('carId').value.trim();
+  
+        try {
+          const response = await fetch('/', {
+            method: 'POST',
+            
+            body: JSON.stringify({
+              task_name: taskName,
+              created_on: createdOn,
+              due_by: dueBy,
+              car_id: carId,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+              },
+          });
+  
+          createTaskModal.hide();
+  
+          if (response.ok) {
+            console.log('New task created');
+            // Handle success, e.g., show a success message or redirect to another page
+            document.location.replace('/');
+          } else {
+            console.error('Failed to create task:', response.status, response.statusText);
+            // Handle failure, e.g., show an error message to the user
+          }
+        } catch (error) {
+          console.error('Error creating task:', error);
+          // Handle unexpected errors
         }
-      } catch (error) {
-        console.error('Error creating task:', error);
-        // Handle unexpected errors
-      }
-    });
-  }
-});
+      });
+    }
+  });
+  
